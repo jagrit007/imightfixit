4858cc92721e0c0ff08c5b402f90222f2bf09c94
Jagrit Thapar <jagritthapar007@gmail.com>
2024-04-28 04:06:45 +0530

    feat: setup routes requiring signed in status

--- src/App.js
+++ src/App.js
@@ -33,11 +33,11 @@ function App() {
         <Route path='/services' element={<Services/>}/>
         {isUserSignedIn && isUserAdmin && <Route path='/admin' element={<AdminPage/>}/>}
 
-        <Route path='/UserDashboard' element={<UserDashboard/>}/>
+        {isUserSignedIn && <Route path='/UserDashboard' element={<UserDashboard/>}/>}
         {isUserSignedIn && <Route path='/account' element={<Account />}/>}
         <Route path='/about' element={<About />}/>
-        <Route path='/checkout' element={<Checkout />}/>
-        <Route path='/thanks' element={<Thanks />}/>
+        {isUserSignedIn && <Route path='/checkout' element={<Checkout />}/>}
+        {isUserSignedIn && <Route path='/thanks' element={<Thanks />}/>}
       </Routes>
       <Footer />
     </div>
