9b159af90a415ef955dae7ada857f9246618e105
IceBerG-15 <warmachiness13@gmail.com>
2024-04-27 17:25:51 +0530

    feat: login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -40,27 +40,36 @@ const Login = () => {
 
 
   return (
-    <div className='w-full h-screen flex'>
-        <div className='w-[50%] h-[100%] bg-[#1a1a1a] text-white flex justify-center items-center'>
-            <form onSubmit={handleLogin} className='text-center border rounded-lg w-[600px] h-[400px] p-9'>
-            {/* Inputs */}
-            <label>Username</label>
-            <br/>
-            <input className='w-[400px] h-[40px] rounded-xl bg-zinc-700 p-2' type="text" placeholder='Enter your username' value={username} onChange={(e)=>{setUsername(e.target.value)}}></input>
-            <br/>
-            <br/>
-            <label>Password</label>
-            <br/>
-            <input className='w-[400px] h-[40px] rounded-xl bg-zinc-700 p-2' type="password" placeholder='Enter your password' value={password} onChange={(e)=>{setPassword(e.target.value)}}></input>
-            <br/>
-            <br/>
-            <button type="submit" className='w-[200px] h-[50px] border hover:bg-teal-900'>Login</button>
-            </form>
-        </div>
-        <div className='w-[50%] h-[100%] flex justify-center items-center bg-teal-800'>
-            <h2 className='text-4xl text-white'>Login 🧑🏻‍💻</h2>
-        </div>
-    </div>
+    // <!-- component -->
+<div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+	<div class="relative py-3 sm:max-w-xl sm:mx-auto">
+		<div
+			class="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+		</div>
+		<div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+			<div class="max-w-md mx-auto">
+				<div>
+					<h1 class="text-2xl font-semibold">Login Form with Floating Labels</h1>
+				</div>
+				<div class="divide-y divide-gray-200">
+					<div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+						<div class="relative">
+							<input autocomplete="off" id="email" name="email" type="text" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" />
+							<label for="email" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
+						</div>
+						<div class="relative">
+							<input autocomplete="off" id="password" name="password" type="password" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
+							<label for="password" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+						</div>
+						<div class="relative">
+							<button class="bg-blue-500 text-white rounded-md px-2 py-1">Submit</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
   )
 }
 
