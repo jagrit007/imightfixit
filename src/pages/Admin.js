6a5d54a674b254ce8121d753e1b9170b55f1e405
Jagrit Thapar <jagritthapar007@gmail.com>
2024-04-28 05:18:22 +0530

    feat: api interactions

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,7 +1,9 @@
-import React, { useState,useEffect  } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios'; // Import Axios library
+import { useNavigate } from 'react-router-dom';
 
 const AdminPage = () => {
+  const navigate = useNavigate();
   const userToken = localStorage.getItem('token');
   const [serviceType, setServiceType] = useState('');
   const [showBookingModal, setShowBookingModal] = useState(false);
@@ -42,6 +44,29 @@ const AdminPage = () => {
     fetchServiceCards();
   }, []);
 
+  const [currentOrders, setCurrentOrders] = useState([]);
+  const [orderHistory, setOrderHistory] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/order', {
+          headers: {
+            'Authorization': `${userToken}`
+          }
+        });
+        const data = await response.json();
+        if (data.status === 'success') {
+          setCurrentOrders(data.data.filter(order => order.status === 'pending' || order.status === 'in_progress'));
+          setOrderHistory(data.data.filter(order => order.status === 'completed' || order.status === 'cancelled'));
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
   function fetchServiceCards() {
     axios.get('http://localhost:5000/service/getAll')
       .then(response => {
@@ -65,6 +90,83 @@ const AdminPage = () => {
       setSavedService(null);
     }
   }, [savedService, services]);
+  
+  const completeOrder = async (orderId) => {
+    try {
+      const response = await fetch(`http://localhost:5000/order/finish/${orderId}`, {
+        method: 'PATCH',
+        headers: {
+          'Authorization': `${userToken}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          order_status: 'completed'
+        })
+      });
+      const data = await response.json();
+      console.log(data); // Logging the response for verification
+      // Update currentOrders state to reflect the updated status
+      setCurrentOrders(prevOrders =>
+        prevOrders.filter(order => order.id !== orderId)
+      );
+      // Move the completed order to orderHistory
+      setOrderHistory(prevOrders => [...prevOrders, ...currentOrders.filter(order => order.id === orderId)]);
+    } catch (error) {
+      console.error('Error completing order:', error);
+    }
+  };
+  
+  const cancelOrder = async (orderId) => {
+    try {
+      const response = await fetch(`http://localhost:5000/order/finish/${orderId}`, {
+        method: 'PATCH',
+        headers: {
+          'Authorization': `${userToken}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          order_status: 'cancelled'
+        })
+      });
+      const data = await response.json();
+      console.log(data); // Logging the response for verification
+      // Update currentOrders state to remove the cancelled order
+      setCurrentOrders(prevOrders =>
+        prevOrders.filter(order => order.id !== orderId)
+      );
+      // Move the cancelled order to orderHistory
+      setOrderHistory(prevOrders => [...prevOrders, ...currentOrders.filter(order => order.id === orderId)]);
+    } catch (error) {
+      console.error('Error cancelling order:', error);
+    }
+  };
+  
+  const acceptOrder = async (orderId) => {
+    try {
+      const response = await fetch(`http://localhost:5000/order/accept/${orderId}`, {
+        method: 'PATCH',
+        headers: {
+          'Authorization': `${userToken}`,
+          "total_price": prompt("Enter total_price for this service:")
+        },
+      });
+      const data = await response.json();
+      alert(`Order with ID: ${orderId} accepted successfully!`)
+      console.log(data); // Logging the response for verification
+      // Update currentOrders state to reflect the updated status
+      setCurrentOrders(prevOrders =>
+        prevOrders.map(order =>
+          order.id === orderId
+            ? { ...order, status: 'in_progress' }
+            : order
+        )
+      );
+      navigate('/admin');
+    } catch (error) {
+      console.error('Error accepting order:', error);
+    }
+  };
+  
   
   const handleSaveService = () => {
     // Create an object with service details
@@ -129,13 +231,42 @@ const AdminPage = () => {
       });
   }, []); // Empty dependency array means this effect runs once after the component mounts
 
-  const renderAcceptOrCompleteButton = (status) => {
+  const renderAcceptOrCompleteButton = (status, orderId) => {
     if (status === 'in_progress') {
-        return <button className="relative inline-flex text-sm sm:text-base bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg z-50">Complete</button>;
-    } else {
-        return <button className="relative inline-flex text-sm sm:text-base bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg z-50">Accept</button>;
-        
+      return (
+        <button onClick={() => completeOrder(orderId)} className="relative inline-flex text-sm sm:text-base bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg z-50">Complete</button>
+      );
+    } else if (status === 'pending') {
+      return (
+        <button onClick={() => acceptOrder(orderId)} className="relative inline-flex text-sm sm:text-base bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg z-50">Accept</button>
+      );
+    } else if (status === 'completed') {
+      return (
+        <button className="relative inline-flex text-sm sm:text-base bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-lg z-50" disabled>Completed</button>
+      );
     }
+  };
+
+  const renderCancelOrCancelButton = (status, orderId) => {
+    if (status === 'in_progress' || status === 'pending') {
+      return (
+        <button onClick={() => cancelOrder(orderId)} className="relative inline-flex text-sm sm:text-base bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-lg z-50">Cancel</button>
+      );
+    } else {
+      return (
+        <button className="relative inline-flex text-sm sm:text-base bg-gray-500 text-white font-bold py-2 px-4 rounded-lg z-50" disabled>Cancelled</button>
+      );
+    }
+  };
+
+  const options = { 
+    year: 'numeric', 
+    month: 'long', 
+    day: 'numeric', 
+    hour: 'numeric', 
+    minute: 'numeric', 
+    second: 'numeric', 
+    timeZone: 'UTC' 
   };
 
   return (
@@ -159,7 +290,7 @@ const AdminPage = () => {
           <ServiceCard
             key={index}
             title={service.name}
-            icon={<svg className="w-12 h-12 mx-auto text-gray-400 sm:mx-0" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21 4H3C2.46957 4 1.96086 4.21071 1.58579 4.58579C1.21071 4.96086 1 5.46957 1 6V18C1 18.5304 1.21071 19.0391 1.58579 19.4142C1.96086 19.7893 2.46957 20 3 20H21C21.5304 20 22.0391 19.7893 22.4142 19.4142C22.7893 19.0391 23 18.5304 23 18V6C23 5.46957 22.7893 4.96086 22.4142 4.58579C22.0391 4.21071 21.5304 4 21 4Z" stroke="#111827" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/><path d="M9 20V12H15V20" stroke="#111827" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/><path d="M9 12V6H15V12" stroke="#111827" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/></svg>}
+            icon={<svg className="w-12 h-12 mx-auto text-gray-400 sm:mx-0" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 2C3.45 2 3 2.45 3 3V21C3 21.55 3.45 22 4 22H20C20.55 22 21 21.55 21 21V3C21 2.45 20.55 2 20 2H4ZM6 20H4V18H6V20ZM6 16H4V4H6V16ZM8 20H6V18H8V20ZM8 16H6V14H8V16ZM8 12H6V10H8V12ZM12 20H10V18H12V20ZM12 16H10V14H12V16ZM12 12H10V10H12V12ZM16 20H14V18H16V20ZM16 16H14V14H16V16ZM16 12H14V10H16V12ZM20 20H18V18H20V20ZM20 16H18V14H20V16Z" fill="#111827"/></svg>}
             description={service.description}
             priceRange={"₹" + service.price}
             duration={"Number of Days: " + service.duration}
@@ -168,31 +299,40 @@ const AdminPage = () => {
       </div>
         </div>
       </section>
-    <div class="mt-32">
-        <div class="px-4 sm:px-8 max-w-5xl m-auto">
-            <h1 class="text-center font-semibold text-sm">Current Orders</h1>
-            <p class="mt-2 text-center text-xs mb-4 text-gray-500">Last 10 Orders</p>
-            <ul class="border border-gray-200 rounded overflow-hidden shadow-md">
-            <li class="flex justify-between px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out">
-            <span>First Item</span>
-            <div className="flex space-x-4">
-                    {renderAcceptOrCompleteButton(status)}
-                    <button className="relative inline-flex text-sm sm:text-base bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg z-50">Cancel</button>
-                  </div>
-            </li>
-            </ul>
+      <div>
+      <div className="mt-32">
+        <div className="px-4 sm:px-8 max-w-5xl m-auto">
+          <h1 className="text-center font-semibold text-sm">Current Orders</h1>
+          <p className="mt-2 text-center text-xs mb-4 text-gray-500">Last 10 Orders</p>
+          <ul className="border border-gray-200 rounded overflow-hidden shadow-md">
+            {currentOrders.map(order => (
+              <li key={order.id} className="flex justify-between px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out">
+                <span>Service Order: <strong>{new Date(order.created_at).toLocaleDateString('en-US', options)}</strong></span>
+                <span>Order ID: <strong>{(order.id)}</strong></span>
+                <div className="flex space-x-4">
+                  {renderAcceptOrCompleteButton(order.status, order.id)}
+                  {renderCancelOrCancelButton(order.status, order.id)}
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
-    </div>
-    <div class="mt-32">
-        <div class="px-4 sm:px-8 max-w-5xl m-auto">
-            <h1 class="text-center font-semibold text-sm">Order History</h1>
-            <p class="mt-2 text-center text-xs mb-4 text-gray-500">Last 10 Orders</p>
-            <ul class="border border-gray-200 rounded overflow-hidden shadow-md">
-            <li class="flex justify-between px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out">
-            <span>First Item</span>
-            </li>
-            </ul>
+      </div>
+      <div className="mt-32 mb-8">
+        <div className="px-4 sm:px-8 max-w-5xl m-auto">
+          <h1 className="text-center font-semibold text-sm">Order History</h1>
+          <p className="mt-2 text-center text-xs mb-4 text-gray-500">Last 10 Orders</p>
+          <ul className="border border-gray-200 rounded overflow-hidden shadow-md">
+            {orderHistory.map(order => (
+              <li key={order.id} className="flex justify-between px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out">
+              <span>Service Order: <strong>{new Date(order.created_at).toLocaleDateString('en-US', options)}</strong></span>
+              <span>Order ID: <strong>{(order.id)}</strong></span>
+              <span>Order Status: <strong>{(order.status)}</strong></span>
+              </li>
+            ))}
+          </ul>
         </div>
+      </div>
     </div>
       {/* Modal component */}
       {showBookingModal && (
@@ -210,19 +350,18 @@ const AdminPage = () => {
                 </div>
                 <div>
                   <label htmlFor="description" className="block text-sm font-medium text-blue-700">Description</label>
-                  <textarea id="description" value={description} onChange={(e) => setDescription(e.target.value)} rows="4" autoComplete="off" required className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:ring-purple-500 focus:border-purple-500"></textarea>
+                  <input type="text" id="description" value={description} onChange={(e) => setDescription(e.target.value)} autoComplete="off" required className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:ring-purple-500 focus:border-purple-500" />
                 </div>
                 <div>
                   <label htmlFor="priceRange" className="block text-sm font-medium text-blue-700">Price Range</label>
-                  <input type="text" id="priceRange" value={priceRange} onChange={(e) => setPriceRange(e.target.value)} autoComplete="off" required className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:ring-purple-500 focus:border-purple-500" />
+                  <input type="number" id="priceRange" value={priceRange} onChange={(e) => setPriceRange(e.target.value)} autoComplete="off" required className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:ring-purple-500 focus:border-purple-500" />
                 </div>
                 <div>
-                  <label htmlFor="duration" className="block text-sm font-medium text-blue-700">Enter Number of Days</label>
-                  <input type="text" id="priceRange" value={duration} onChange={(e) => setDuration(e.target.value)} autoComplete="off" required className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:ring-purple-500 focus:border-purple-500" />
+                  <label htmlFor="duration" className="block text-sm font-medium text-blue-700">Duration (in Days)</label>
+                  <input type="number" id="duration" value={duration} onChange={(e) => setDuration(e.target.value)} autoComplete="off" required className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:ring-purple-500 focus:border-purple-500" />
                 </div>
-                <div className="flex justify-end mt-4">
-                  <button type="button" onClick={closeBookingForm} className="mr-4 px-6 py-2 bg-gray-900 hover:bg-gray-800 text-white font-bold rounded focus:outline-none focus:shadow-outline">Cancel</button>
-                  <button type="button" onClick={handleSaveService} className="px-6 py-2 bg-gray-900 hover:bg-gray-800 text-white font-bold rounded focus:outline-none focus:shadow-outline">Save</button>
+                <div className="pt-4">
+                  <button onClick={handleSaveService} type="button" className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-blue-500 border border-transparent rounded-md hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500">Save Service</button>
                 </div>
               </form>
             </div>
@@ -232,17 +371,19 @@ const AdminPage = () => {
     </div>
   );
 }
-function ServiceCard({ title, description, icon , priceRange, duration}) {
-    return (
-      <div className="overflow-hidden bg-white shadow-md rounded-xl">
-        <div className="p-9">
-          <div className="flex justify-center mb-6">{icon}</div>
-          <h3 className="mt-6 text-xl font-bold text-gray-900 sm:mt-10">{title}</h3>
-          <p className="mt-6 text-base text-gray-600">{description}</p>
-          <p className="mt-6 text-base text-gray-600">{duration}</p>
-          <p className="mt-6 text-base font-bold text-blue-600">{priceRange}</p>
-        </div>
-      </div>
-    );
-  }
+
+const ServiceCard = ({ title, icon, description, priceRange, duration }) => (
+  <div className="flex items-center flex-col p-8 bg-white rounded-lg shadow-md hover:shadow-lg">
+    <div className="flex-shrink-0">
+      {icon}
+    </div>
+    <div className="mt-4">
+      <h3 className="text-lg font-medium leading-6 text-gray-900">{title}</h3>
+      <p className="mt-2 text-base leading-6 text-gray-500">{description}</p>
+      <p className="mt-2 text-base leading-6 text-gray-500">{priceRange}</p>
+      <p className="mt-2 text-base leading-6 text-gray-500">{duration}</p>
+    </div>
+  </div>
+);
+
 export default AdminPage;
