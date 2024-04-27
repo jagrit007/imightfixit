a3c1446f3aab272f1c36731e2207922f8a52351f
Jagrit Thapar <jagritthapar007@gmail.com>
2024-04-27 19:02:38 +0530

    lowercase endpoint

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         <Route path='/' element={<Home />}/>
         <Route path='/login' element={<Login />}/>
         <Route path='/signup' element={<SignUp />}/>
-        <Route path='/Reviews' element={<Reviews />}/>
+        <Route path='/reviews' element={<Reviews />}/>
         {isUserSignedIn && <Route path='/account' element={<Account />}/>}
       </Routes>
       <Footer />
