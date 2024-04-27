c687f714593717da31c97e4697f981843e0b73b9
IceBerG-15 <warmachiness13@gmail.com>
2024-04-27 22:26:12 +0530

    fix: footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -45,7 +45,7 @@ const Footer = () => {
     <div class="flex flex-wrap items-center md:justify-between justify-center">
       <div class="w-full md:w-4/12 px-4 mx-auto text-center">
         <div class="text-sm text-blueGray-500 font-semibold py-1">
-          Copyright © <span id="get-current-year">2021</span>.
+          Copyright © DeezNodes <span id="get-current-year">2024</span>.
         </div>
       </div>
     </div>
