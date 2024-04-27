09403e9d0748d3e7b90ae59d7f10d9ecb33c985d
IceBerG-15 <warmachiness13@gmail.com>
2024-04-27 16:42:00 +0530

    new commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   return (
-    <div className='w-full h-screen flex items-center justify-center bg-[#1a1a1a] text-white'>
-        <h1 className='text-5xl text-white'>HOMEPAGE 🤠</h1>
-        </div>
-  )
+    <div className="home">
+      <h1>Welcome to Mobile Repair Shop</h1>
+      <p>High-quality mobile repair services at affordable prices.</p>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
