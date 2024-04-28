f6f9f1b5abc2bd6f7a66da87c57fb6ed9321a750
Jagrit Thapar <jagritthapar007@gmail.com>
2024-04-28 11:24:05 +0530

    refac: class to className

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,28 +5,28 @@ import AboutSection from '../components/About';
 const Home = () => {
   return (
     <div>
-    <div class="bg-gray-900 py-20">
-    <div class="container mx-auto px-6 md:px-12">
-        <div class="flex flex-col md:flex-row items-center">
-            <div class="md:w-1/2 lg:w-2/3">
-                <h1 class="text-4xl md:text-6xl lg:text-7xl text-white font-bold mb-6">
-                    Welcome to our <br class="hidden md:block" />
-                    <span class="text-indigo-500">iMayFixIt</span> Website
+    <div className="bg-gray-900 py-20">
+    <div className="container mx-auto px-6 md:px-12">
+        <div className="flex flex-col md:flex-row items-center">
+            <div className="md:w-1/2 lg:w-2/3">
+                <h1 className="text-4xl md:text-6xl lg:text-7xl text-white font-bold mb-6">
+                    Welcome to our <br className="hidden md:block" />
+                    <span className="text-indigo-500">iMayFixIt</span> Website
                 </h1>
-                <p class="text-lg md:text-xl lg:text-2xl text-gray-400 mb-8">
+                <p className="text-lg md:text-xl lg:text-2xl text-gray-400 mb-8">
                 Don't Worry,I May Fix It—Probably!
                 </p>
-                <div class="flex gap-2">
-                    <a href="/login" class="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-md">Login/Register
+                <div className="flex gap-2">
+                    <a href="/login" className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-md">Login/Register
                     </a>
-                    <a href="/about" class="bg-gray-700 hover:bg-gray-600 text-white font-bold py-3 px-6 rounded-md">Learn More
+                    <a href="/about" className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-3 px-6 rounded-md">Learn More
                     </a>
                 </div>
             </div>
         
 
-          <div class="md:w-1/2 lg:w-1/3 mt-8 md:mt-0">
-              <img src="home.jpg" alt="image description" class="h-auto max-w-xl rounded-lg shadow-xl shadow-xl-right dark:shadow-blue-800"/>
+          <div className="md:w-1/2 lg:w-1/3 mt-8 md:mt-0">
+              <img src="home.jpg" alt="image description" className="h-auto max-w-xl rounded-lg shadow-xl shadow-xl-right dark:shadow-blue-800"/>
           </div>
 
         </div>       
